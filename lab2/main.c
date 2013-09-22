16f71301ce68715f25e18997637600181727c606
Frostjaw <illuzive13@gmail.com>
2013-09-22 23:09:20 +0300

    add queueing

--- lab2/main.c
+++ lab2/main.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
     char *host = argv[1];
     char *port = argv[2];
 
-    int socket_descriptor = create_tcp_server(host, atoi(port), 1);
+    int socket_descriptor = create_tcp_server(host, atoi(port), 10);
 
 	if(socket_descriptor == -1)
 	{
@@ -30,28 +30,31 @@ int main(int argc, char **argv)
 		return 0;
 	}
 
-    printf("Awaiting connection...\n");
-
-    int accepted_socket;
-
-    do {
-        accepted_socket = accept(socket_descriptor, NULL, NULL);
-    }
-    while (accepted_socket == -1);
-
-    printf("Connected.\n");
-
-    int bytes_read = 0;
-
     while (1) {
-        bytes_read = recv(accepted_socket, buffer, BUFFER_SIZE, 0);
+		printf("Awaiting connection...\n");
 
-        if (bytes_read <= 0) {
-            printf("Disconnected.\n");
-            break;
-        }
+		int accepted_socket = accept(socket_descriptor, NULL, NULL);
 
-        send(accepted_socket, buffer, bytes_read, 0);
+		if(accepted_socket == -1)
+		{
+			perror("accept() error");
+			continue;
+		}
+
+		printf("Connected.\n");
+
+		while(1)
+		{
+        	int bytes_read = recv(accepted_socket, buffer, BUFFER_SIZE, 0);
+
+        	if (bytes_read <= 0) {
+            	printf("Disconnected.\n");
+				close(accepted_socket);
+            	break;
+        	}
+
+        	send(accepted_socket, buffer, bytes_read, 0);
+		}
     }
 
     close(socket_descriptor);
