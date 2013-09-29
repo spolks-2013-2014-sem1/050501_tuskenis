324b152d913cd9bc001f1cede5d8acbe53511cf5
Frostjaw <illuzive13@gmail.com>
2013-09-29 13:54:12 +0300

    add SIGINT handling

--- lab2/main.c
+++ lab2/main.c
@@ -6,17 +6,27 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
+#include <signal.h>
 
 #define BUFFER_SIZE 6
 unsigned char buffer[BUFFER_SIZE];
 
+int quit_flag = 0;
+
+void sig_handler(int i)
+{
+	quit_flag = 1;
+}
+
 int main(int argc, char **argv)
 {
     if (argc < 3) {
-        printf("\n Usage: stcps host port");
-        printf("\n    e.g.: ./stcps 127.0.0.1 6677\n\n");
+        printf("\n Usage: main host port");
+        printf("\n    e.g.: ./main 127.0.0.1 6677\n\n");
         return 0;
     }
+
+	set_sig_handler(SIGINT, sig_handler);
 
     char *host = argv[1];
     char *port = argv[2];
@@ -28,7 +38,7 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    while (1) {
+    while (quit_flag == 0) {
         printf("Awaiting connection...\n");
 
         int accepted_socket = accept(socket_descriptor, NULL, NULL);
@@ -44,7 +54,7 @@ int main(int argc, char **argv)
             int bytes_read = recv(accepted_socket, buffer, BUFFER_SIZE, 0);
 
             if (bytes_read <= 0) {
-                printf("Disconnected.\n");
+                perror("recv() error");
                 close(accepted_socket);
                 break;
             }
