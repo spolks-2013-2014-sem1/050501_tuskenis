6c2f6cd12dfbc2c698448d2275d760ffc1c1054f
Frostjaw <illuzive13@gmail.com>
2013-10-18 19:13:45 +0300

    split client and server

---
+++ lab4/client.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <error.h>
+#include <signal.h>
+#include "../lib-spolks/tcp_wrap.h"
+#include "../lib-spolks/utils.h"
+
+#define BUFFER_SIZE 256
+
+int send_file(char *filepath, int socket_descriptor)
+{
+	int totalBytesSent = 0;
+    char buffer[BUFFER_SIZE];
+    FILE *fd;
+    int file_size = fsize(filepath);
+    int bytes_read;
+
+    fd = fopen(filepath, "r");
+
+    if (fd == NULL || file_size < 1)
+        return -1;
+
+	itoa(file_size, buffer);
+
+	char *filename = parse_filename(filepath);
+    send(socket_descriptor, filename, strlen(filename), 0);	// Send file name
+	free(filename);
+    send(socket_descriptor, buffer, strlen(buffer), 0); 	// Send file size
+
+	// number of file data sending iterations between sending out-of-band data
+	int oob_count = (file_size / BUFFER_SIZE) / 5;
+	
+	// Send file data
+	int i = 0;	// file data sending iterations counter
+    while (1) {
+        bytes_read = fread(buffer, sizeof(unsigned char), BUFFER_SIZE, fd);
+
+        if (bytes_read <= 0)
+            break;
+
+        if(send(socket_descriptor, buffer, bytes_read, 0) == -1) {
+			fclose(fd);
+			return -1;
+		}
+
+		totalBytesSent += bytes_read;
+
+		// Generate out-of-band data
+		if(oob_count > 0 && ++i % oob_count == 0) {
+			printf("sent %d bytes\n", totalBytesSent);
+
+			if(send(socket_descriptor, "Z", 1, MSG_OOB) == -1)
+				perror("send OOB error");
+		}
+
+        if (bytes_read < BUFFER_SIZE)
+            break;
+    }
+
+	printf("Total bytes sent: %d\n", totalBytesSent);
+    fclose(fd);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 4) {
+        printf("\n\tUsage: %s <filepath> <host> <port>\n\n", argv[0]);
+        return 0;
+    }
+
+    char *filepath = argv[1];
+    char *host = argv[2];
+    char *port = argv[3];
+
+    int socket_descriptor = tcp_connect(host, atoi(port));
+
+   	if (socket_descriptor == -1) {
+		perror("tcp_connect() errno");
+		return 0;
+	}
+
+	if (send_file(filepath, socket_descriptor) == -1)
+       	perror("send_file() errno");
+
+	close(socket_descriptor);
+    return 0;
+}
+
---
+++ lab4/server.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <error.h>
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
+#include "../lib-spolks/tcp_wrap.h"
+#include "../lib-spolks/utils.h"
+
+#define BUFFER_SIZE 256
+
+int quit_flag = 0;
+
+void sig_handler(int i)
+{
+	quit_flag = 1;
+}
+
+void urg_handler(int i)
+{
+	//dummy
+}
+
+int recv_file(int remote_socket)
+{
+	int totalBytesRecieved = 0;
+    int bytes_read;
+    int file_size;
+    char buffer[BUFFER_SIZE];
+    FILE *fd;
+
+    // Recieve filename
+    bytes_read = recv(remote_socket, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes_read < 1)
+        return -1;
+    buffer[bytes_read] = '\0';
+
+    // Create file
+	
+    fd = fopen(buffer, "w");
+    if (fd == NULL)
+        return -1;
+
+    // Recieve file size
+    bytes_read = recv(remote_socket, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes_read < 1)
+        return -1;
+    buffer[bytes_read] = '\0';
+    file_size = atoi(buffer);
+
+    // Recieve file data
+	while(totalBytesRecieved < file_size) {
+        bytes_read = recv(remote_socket, buffer, BUFFER_SIZE, 0);
+
+		if(sockatmark(remote_socket) == 1) {
+			printf("recieved %d bytes\n", totalBytesRecieved);
+
+			char cbuf;
+			if(recv(remote_socket, &cbuf, 1, MSG_OOB) < 1) {
+				perror("recv() OOB error");
+			}
+
+			if(bytes_read < 1)
+				continue;
+		}
+
+		if(bytes_read == 0)
+			break;		
+
+        if (bytes_read == -1) {
+            fclose(fd);
+			return -1;
+		}
+
+        fwrite(buffer, sizeof(unsigned char), bytes_read, fd);
+		totalBytesRecieved += bytes_read; 
+    }
+
+	printf("Total bytes recieved: %d\n", totalBytesRecieved);
+    fclose(fd);
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3) {
+        printf("\n\tUsage: %s <host> <port>\n\n", argv[0]);
+        return 0;
+    }
+
+    char *host = argv[1];
+    char *port = argv[2];
+
+	set_sig_handler(SIGINT, sig_handler);
+	set_sig_handler(SIGURG, urg_handler);
+
+	int server_socket = create_tcp_server(host, atoi(port), 10);
+
+   	if (server_socket == -1) {
+       	perror("create_tcp_server() errno");
+		return 0;
+   	}
+	
+	while (quit_flag == 0) {
+       	int remote_socket = accept(server_socket, NULL, NULL);
+
+       	if (remote_socket == -1) {
+			perror("accept() errno");
+           	continue;
+		}
+
+		if(fcntl(remote_socket, F_SETOWN, getpid()) == -1) 
+			perror("fcntl() error");
+
+       	if (recv_file(remote_socket) == -1)
+           	perror("recv_file() errno");
+
+       	close(remote_socket);
+   	}
+
+   	close(server_socket);
+    return 0;
+}
